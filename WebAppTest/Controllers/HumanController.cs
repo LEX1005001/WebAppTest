e9dea6e42974490b4391457fff6498e3b911de84
LEX1005001 <AleX69.galkin@yandex.ru>
2024-06-13 21:07:23 +0300

    Add project files.

---
+++ WebAppTest/Controllers/HumanController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Data;
+
+namespace WebAppTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HumanController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public HumanController(DataContext context) 
+        {
+            _context = context; 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Human>>> AddHuman(Human human)
+        {
+            _context.Humans.Add(human);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Humans.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Human>>> GetAllHumans()
+        {
+            return Ok(await _context.Humans.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Human>>> GetHuman(int id)
+        {
+            var human = await _context.Humans.FindAsync(id);
+            if (human == null)
+            {
+                return BadRequest("Human not found.");
+            }
+            return Ok(human);
+        }
+    }
+}
---
+++ WebAppTest/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using WebAppTest;
+
+namespace WebApplication3.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Human> Humans => Set<Human>();
+    }
+}
